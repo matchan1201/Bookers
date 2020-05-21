4c20fb7d07b73eaa4e059ef7273b6de5d6f6a5ff
yoshipon1201 <bryo17006@g.nihon-u.ac.jp>
2020-05-22 00:11:00 +0900

    [05/21]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true,
+                   length: { minimum: 2, maximum: 20 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get "home/about" => "home#about"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :create, :show]
+  resources :books, only: [:index, :create, :show, :edit, :destroy]
+  resources :users, only: [:show]
 end
