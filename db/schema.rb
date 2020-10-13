b0cdfe4fd088886bf85b05e417f4a61b0da89e72
matchan1201 <bumpakano.1@gmail.com>
2020-10-13 21:29:05 +0900

    aaaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_002834) do
+ActiveRecord::Schema.define(version: 2020_10_02_004640) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "practices", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
