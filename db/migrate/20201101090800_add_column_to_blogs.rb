3f908c63ceabce8a9e622ebea209c06fb9027c18
matchan1201 <bumpakano.1@gmail.com>
2020-11-01 18:20:54 +0900

    修正

--- db/migrate/20201101090800_add_column_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :author_name, :string
-  end
-end
--- db/migrate/20201101091038_add_column_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :published_at, :string
-  end
-end
